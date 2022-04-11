d006472653ebeeab357fbc7aac1f0e7762cb1499
Alex Verbniak <ov@socprime.com>
2022-04-11 14:47:07 +0300

    -

--- mulesoft-azure-sentinel-data-connector/MuleSoftCloudhubSentinelConnector/rest_api.py
+++ mulesoft-azure-sentinel-data-connector/MuleSoftCloudhubSentinelConnector/rest_api.py
@@ -3,14 +3,14 @@ import requests
 
 
 def get_alerts(username, password, app_name, env_id, start_time, end_time, limit=1000):
-    mock_url = "https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/f1e97bc6-315a-4490-82a7-23abe036327a.anypoint-platform/cloudhub-api/1.0.20/m/v2/alerts?offset=0"
+    #mock_url = "https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/f1e97bc6-315a-4490-82a7-23abe036327a.anypoint-platform/cloudhub-api/1.0.20/m/v2/alerts?offset=0"
     url = 'https://anypoint.mulesoft.com/cloudhub/api/v2/alerts'
     params = {
         "resource": app_name,
         "limit": limit
     }
     headers = {'X-ANYPNT-ENV-ID': env_id}
-    alerts = requests.get(url=mock_url, headers=headers, auth=(username, password), params=params).json()
+    alerts = requests.get(url=url, headers=headers, auth=(username, password), params=params).json()
     filtered_alerts = []
     if type(alerts) == list:
         filtered_alerts = list(filter(lambda alert: start_time <= alert["createdAt"] <= end_time, alerts))
@@ -25,13 +25,13 @@ def get_alerts(username, password, app_name, env_id, start_time, end_time, limit
 
 
 def get_logs(username, password, env_id, start_time, end_time):
-    mock_domain_url = "https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/f1e97bc6-315a-4490-82a7-23abe036327a.anypoint-platform/cloudhub-api/1.0.20/m/v2/applications?retrieveStatistics=false&period=3600000&retrieveLogLevels=true&retrieveTrackingSettings=true&retrieveIpAddresses=true"
+    #mock_domain_url = "https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/f1e97bc6-315a-4490-82a7-23abe036327a.anypoint-platform/cloudhub-api/1.0.20/m/v2/applications?retrieveStatistics=false&period=3600000&retrieveLogLevels=true&retrieveTrackingSettings=true&retrieveIpAddresses=true"
     domain_url = "https://anypoint.mulesoft.com/cloudhub/api/v2/applications"
     headers = {'X-ANYPNT-ENV-ID': env_id,
                "content-type": "application/json"}
     body = {"startTime": start_time,
             "endTime": end_time}
-    domains_json = requests.get(url=mock_domain_url, headers=headers, data=body, auth=(username, password)).json()
+    domains_json = requests.get(url=domain_url, headers=headers, data=body, auth=(username, password)).json()
     logs = []
     if len(domains_json) == 0:
         logging.info("List of domains are empty.")
@@ -39,9 +39,9 @@ def get_logs(username, password, env_id, start_time, end_time):
         if type(domains_json) is list:
             domains = list(map(lambda application: application["domain"], domains_json))
             for domain in domains:
-                mock_log_url = "https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/f1e97bc6-315a-4490-82a7-23abe036327a.anypoint-platform/cloudhub-api/1.0.20/m/v2/applications/" + domain + "/logs"
+                #mock_log_url = "https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/f1e97bc6-315a-4490-82a7-23abe036327a.anypoint-platform/cloudhub-api/1.0.20/m/v2/applications/" + domain + "/logs"
                 log_url = "https://anypoint.mulesoft.com/cloudhub/api/v2/applications/" + domain + "/logs"
-                logs += requests.post(url=mock_log_url, headers=headers, auth=(username, password)).json()
+                logs += requests.post(url=log_url, headers=headers, auth=(username, password)).json()
                 if any(type(log) == str for log in logs):
                     logs = []
                     logging.info("List of logs are empty.")
